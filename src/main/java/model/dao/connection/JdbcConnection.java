7b938bfd377e2968d2319e8a7d2b6aedbfc2cd12
Denys Smishny <denshiksmile@gmail.com>
2017-08-24 15:27:55 +0300

    temp

--- src/main/java/model/dao/connection/JdbcConnection.java
+++ src/main/java/model/dao/connection/JdbcConnection.java
@@ -1,9 +1,27 @@
 package model.dao.connection;
 
+import java.sql.Connection;
+
 /**
  * Created by denglob on 8/18/17.
  */
 public class JdbcConnection implements AutoCloseable {
+
+    private Connection connection;
+    private boolean isTransaction;
+
+    public JdbcConnection(Connection connection) {
+        this.connection = connection;
+        isTransaction = false;
+    }
+
+    public void startTransaction() {
+        try {
+            connection.setAutoCommit(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void close() throws Exception {
---
+++ src/main/java/model/dao/connection/SQLConnector.java
@@ -0,0 +1,10 @@
+package model.dao.connection;
+
+/**
+ * Created by denglob on 8/20/17.
+ */
+public interface SQLConnector {
+    String URL = "jdbc:mysql://localhost:3306/firstproject";
+    String USER = "root";
+    String PASSWORD = "";
+}
